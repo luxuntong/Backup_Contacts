d2d79deb950a35a7378c7519d8d5ed344cf6e07d
cxcx <lplcx@live.com>
2014-08-06 03:30:54 +0800

    clone new work place

--- src/com/backup_contacts/MainActivity.java
+++ src/com/backup_contacts/MainActivity.java
@@ -7,6 +7,10 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    //
+    //
+    //
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
